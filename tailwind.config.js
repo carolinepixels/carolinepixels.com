76823dd1f850c7ef64a204e3622a687cbb5ea70f
CarolinePixels <3105567+carolinepixels@users.noreply.github.com>
2022-12-03 15:29:39 +0000

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -50,23 +50,18 @@ module.exports = {
       secondary_base: '#475569', 
       secondary_light: '#94a3b8', 
       secondary_lightest: '#e2e8f0',
-
       pixels_yellow_light: '#FDF0C2', 
       pixels_yellow_base: '#FFF0A6', 
       pixels_yellow_dark: '#F4DD68', 
-
       pixels_purple_light: '#EDC7DF', 
       pixels_purple_base: '#CBAFD4', 
       pixels_purple_dark: '#BF8BCF', 
-
       pixels_pink_light: '#F8BED0', 
       pixels_pink_base: '#FB7A9D', 
       pixels_pink_dark: '#FB6B85', 
-
       pixels_blue_light: '#ECE6EF', 
       pixels_blue_base: '#C9D3EC', 
       pixels_blue_dark: '#8EA7E6', 
-      
       pixels_green_light: '#CEE6E1', 
       pixels_green_base: '#8FD1CE', 
       pixels_green_dark: '#55CFC6', 
